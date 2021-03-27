fe8addc029e81f7cf8361a8f6a37ed71c9daba96
danielalexvega <danielalexvega@gmail.com>
2021-03-27 11:35:23 -0500

    test commit

--- react-employee-dir/src/utils/API.js
+++ react-employee-dir/src/utils/API.js
@@ -2,8 +2,10 @@ import axios from "axios";
 
 const url ="https://randomuser.me/api/?results=50";
 
-export default {
+const API =  {
   getEmployeeList: function() {
     return axios.get(url);
   }
 };
+
+export default API;
